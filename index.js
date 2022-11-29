a2c47c30f0c329a5908513499afcb78d29876289
Charles Swaleh <charlswall@live.com>
2022-11-29 09:35:25 +0300

    completed functions lab assigment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street){
+  return (street >= 42) ? street - 42 : 42 - street;
+}
+
+
+function distanceFromHqInFeet(street){
+  return distanceFromHqInBlocks(street) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+  return (start > destination) ? (start - destination) * 264 : (destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+  const distance = distanceTravelledInFeet(start, destination)
+  if(distance <= 400){
+    return 0;
+  }
+  else if(distance > 400 && distance < 2000){
+    return ((distance - 400) * 2) / 100;
+  }
+  else if(distance > 2000 && distance < 2500){
+    return 25;
+  }
+  else{
+    return "cannot travel that far"
+  }
+}
